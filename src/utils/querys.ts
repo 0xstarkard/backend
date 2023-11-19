fc33024096aa32dccba9a742e6f0c4aead089601
Erik Valle <erik.valle@gmail.com>
2023-11-19 08:24:58 +0300

    Update querys.ts

--- src/utils/querys.ts
+++ src/utils/querys.ts
@@ -7,3 +7,14 @@ export const gettxHash = async (
    return error ? error : data;  
 };
 
+export const createUsers = async (serial: any, key_public: any, key_private: any, key_smartwallet: any, wallet_backup: any, linkto: any): Promise<any> => {
+ 
+  const { data, error } = await supabase
+    .from("stark_user")
+    .insert([{ 'serial': serial, 'key_public':key_public,  'key_private':key_private, 'key_smartwallet':key_smartwallet, 'wallet_backup':wallet_backup, 'linkto':linkto }])
+    .select();
+    console.log(data); 
+   return error ? error : data;
+  
+};
+ 
