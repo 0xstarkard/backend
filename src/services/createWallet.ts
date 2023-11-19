a46b144f1c8c83d0297068e2604af938b06e040d
Erik Valle <erik.valle@gmail.com>
2023-11-19 08:23:19 +0300

    add rednfc
    add rednfc

--- src/services/createWallet.ts
+++ src/services/createWallet.ts
@@ -1,19 +1,21 @@
 import { Request, Response } from "express";
 import {  constants, ec, stark, Account, Provider, hash, CallData, Contract, cairo } from "starknet";
-
+import { createUsers } from "../utils/querys";
 export const createWallet = async (
   req: Request,
   res: Response
 ) => {
-  var id=req.query['idn'];
+  const serial=req.query['serial'];
+  const walletbackup=req.query['walletbackup'];
+  const linkto=req.query['linkto'];
   
   // connect provider
 const provider = new Provider({ sequencer: { network: constants.NetworkName.SN_GOERLI } });
 
 // new Open Zeppelin account v0.5.1
 // Generate public and private key pair.
-const randadd = id;
-const privateKey = randadd;
+
+const privateKey = stark.randomAddress();
 const starkKeyPub = ec.starkCurve.getStarkKey(privateKey as string);
 const OZaccountClassHash = "0x2794ce20e5f2ff0d40e632cb53845b9f4e526ebd8471983f7dbd355b721d5a";
 // Calculate future address of the account
@@ -25,43 +27,8 @@ const OZcontractAddress = hash.calculateContractAddressFromHash(
   "1"
 );
 
-// send funds
-console.log('Precalculated account address=', OZcontractAddress);
-const erc20Address = "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7";
-const { abi: testAbi } = await provider.getClassAt(erc20Address);
-if (testAbi === undefined) { throw new Error("no abi.") };
-// Create a new erc20 contract object
-const erc20 = new Contract(testAbi, erc20Address, provider);
-
-const account = new Account(provider, "0x0494cA5Fcf3861774550bb72d6be7DA08Df1857838A111f64987D135AaF5DE43", process.env.ARGENT, "1");
-
-erc20.connect(account);
-
-console.log(`Invoke Tx - Transfer 10 tokens back to erc20 contract...`);
-const toTransferTk = cairo.uint256(0.01);
-const transferCallData = erc20.populate("transfer", {
-    recipient: OZcontractAddress,
-    amount: toTransferTk // with Cairo 1 contract, 'toTransferTk' can be replaced by '10n'
-});
-    const { transaction_hash: transferTxHash } = await erc20.transfer( transferCallData.calldata);
-
-// Wait for the invoke transaction to be accepted on Starknet
-console.log(`Waiting for Tx to be Accepted on Starknet - Transfer...`);
-await provider.waitForTransaction(transferTxHash);
-
-// Deploy Account
-const OZaccount = new Account(provider, OZcontractAddress, privateKey as string, "1");
-
-const { transaction_hash, contract_address } = await OZaccount.deployAccount({
-    classHash: OZaccountClassHash,
-    constructorCalldata: OZaccountConstructorCallData,
-    addressSalt: starkKeyPub
-});
-
-await provider.waitForTransaction(transaction_hash);
-console.log('✅ New OpenZeppelin account created.\n   address =', contract_address);
-
 //guardar en la db 
-res.status(200).send({ wallet: OZcontractAddress, privatekey:randadd, publickey: starkKeyPub  });
+createUsers(serial, starkKeyPub, privateKey, OZcontractAddress,walletbackup , linkto )
+res.status(200).send({ wallet: OZcontractAddress, privatekey:privateKey, publickey: starkKeyPub  });
   
 };
--- src/services/readNFC.ts
+++ src/services/readNFC.ts
@@ -1,27 +1,54 @@
 import { Request, Response } from "express";
-import { Account, constants, ec, json, stark, Provider, hash, CallData } from "starknet";
-  
-export const readNFC = async (req: Request, res: Response) => { 
-    var id=req.query['idn'];
-  
-    // connect provider
-const provider = new Provider({ sequencer: { network: constants.NetworkName.SN_GOERLI } });
+import { Account, constants, ec, json, stark, Provider, hash, CallData, Contract , cairo} from "starknet";
 
-// new Open Zeppelin account v0.5.1
-// Generate public and private key pair.
-const randadd = stark.randomAddress();
-const privateKey = randadd;
-const starkKeyPub = ec.starkCurve.getStarkKey(privateKey);
-const OZaccountClassHash = "0x2794ce20e5f2ff0d40e632cb53845b9f4e526ebd8471983f7dbd355b721d5a";
-// Calculate future address of the account
-const OZaccountConstructorCallData = CallData.compile({ publicKey: starkKeyPub });
-const OZcontractAddress = hash.calculateContractAddressFromHash(
-    starkKeyPub,
-    OZaccountClassHash,
-    OZaccountConstructorCallData,
-    "1"
+export const readNFC = async (req: Request, res: Response) => { 
+    const from=req.query['from'];
+    const to=req.query['to'];
+    const amount=req.query['amount'];
+    
+
+    //code for sed tx of starknet
+    // Check balance - should be 100
+const balanceInitial = await erc20.balanceOf(from);
+console.log("from has a balance of:", uint256.uint256ToBN(balanceInitial.balance).toString()); // Cairo 0 response
+
+// Mint amount tokens to account address
+const amountToMint = cairo.uint256(amount);
+console.log("Invoke Tx - Minting  tokens to account0...");
+const { transaction_hash: mintTxHash } = await erc20.mint(
+    from,
+    amountToMint,
+    { maxFee: 900_000_000_000_000 }
 );
 
-console.log('Precalculated account address=', OZcontractAddress);
-res.status(200).send({ wallet: OZcontractAddress, privatekey:randadd, publickey: starkKeyPub  });
+// Wait for the invoke transaction to be accepted on Starknet
+console.log(`Waiting for Tx to be Accepted on Starknet - Minting...`);
+await provider.waitForTransaction(mintTxHash);
+
+// Check balance - should be 1100
+console.log(`Calling Starknet for account balance...`);
+const balanceBeforeTransfer = await erc20.balanceOf(account0.address);
+console.log("account0 has a balance of:", uint256.uint256ToBN(balanceBeforeTransfer.balance).toString()); // Cairo 0 response
+
+// Execute tx transfer of 10 tokens
+console.log(`Invoke Tx - Transfer 10 tokens back to erc20 contract...`);
+const toTransferTk: Uint256 = cairo.uint256(amount);
+const transferCallData: Call = erc20.populate("transfer", {
+    recipient: to,
+    amount: toTransferTk // with Cairo 1 contract, 'toTransferTk' can be replaced by '10n'
+});
+    const { transaction_hash: transferTxHash } = await erc20.transfer( transferCallData.calldata);
+
+// Wait for the invoke transaction to be accepted on Starknet
+console.log(`Waiting for Tx to be Accepted on Starknet - Transfer...`);
+await provider.waitForTransaction(transferTxHash);
+
+// Check balance after transfer - should be 1090
+console.log(`Calling Starknet for account balance...`);
+const balanceAfterTransfer = await erc20.balanceOf(account0.address);
+console.log("account0 has a balance of:", uint256.uint256ToBN(balanceAfterTransfer.balance).toString()); // Cairo 0 response
+
+
+
+res.status(200).send({result:"ok"});
 };
